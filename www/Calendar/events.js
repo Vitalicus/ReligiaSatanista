ae1b3f7ff9c63b14915ae57c270392d51930cc64
Vitalicus <vitalicus@live.com>
2015-01-23 20:37:30 +0200

    new

---
+++ www/Calendar/events.js
@@ -0,0 +1,18 @@
+﻿events = new Array(
+	["Y",	"2",	"19",	"2014",	"",	"",	"Sfintele mucenice 'Pussy Riot'",	"Ziua când pentru prima dată au intrat în biserica ortodoxî și au interpretat un cântec."],
+	["Y",	"7",	"3",	"2014",	"",	"",	"Mucenicul Assange",	"Julian Paul Assange este un ziarist australian, editor și activist pe internet. Este cunoscut drept purtătorul de cuvânt și editorul-sef al WikiLeaks, site ce prezintă scurgeri de informații. (Născut la 3 iulie 1971)"],
+	["Y",	"6",	"21",	"2014",	"",	"",	"Preafericitul Edward",	"Edward Snowden, fost agent CIA, care a divulgat informațiile secrete despre proiectul PRISM și despre urmărirea de către agențiile secrete a multor persoane din toata lumea. (Nascut la 21 iunie 1983)"],
+	["Y",	"3",	"20",	"2014",	"",	"",	"Sfântul Alexandru Ioan Cuza",	"Primul domnitor al Principatelor Unite și formatorul statul român modern (laic)."],
+	["Y",	"12",	"3",	"2014",	"",	"",	"Preafericitul Sfântul Ozzy",	"Este solistul vocal și fondatorul celei mai cunoscute formații de heavy metal din toate timpurile, Black Sabbath"],
+	["Y",	"10",	"31",	"2013",	"",	"",	"Haloween",	"Cea mai mare sărbatoare a 'Religiei Sataniste'. Ziua când toată lumea se maschează în Satani, draci, vrăjitori"],
+	["Y",	"4",	"11",	"2013",	"",	"",	"Preafericitul Sfântul Lavey",	"Ziua de naștere a fondatorului Satanismului Modern, Anton Szandor LaVey. Creatorul cultului 'Biserica lui Satan'"],
+	["Y",	"11",	"19",	"2013",	"",	"",	"Sfântul Mihailo",	"Mihail Vasilievici Lomonosov, fizician, chimist, astronom rus. "],
+	["Y",	"10",	"18",	"2013",	"",	"",	"Preslavita grupa Ghost",	"În semn de recunostință față de slavă lui Satan, adusă prin cântecele grupei suedeze Ghost (Ghost B.C.) clericii acestei grupe au fost canonizați la rang de sfinți. Primul album a fost lansat pe 18 oct. 2010 "],
+	["Y",	"1",	"15",	"2014",	"",	"",	"Sfântul Mihai",	"Mihai Eminescu,  poet, prozator și jurnalist român, socotit de cititorii români și de critica literară postumă drept cea mai importantă voce poetică din literatura română. Spre sfârșitul vieții a devenit ateu fundamentalist"],
+	["Y",	"10",	"07",	"2014",	"",	"",	"Sfântul Nicolai",	"Nikola Tesla a fost un inventator, fizician, inginer mecanic, inginer electrician și unul dintre promotorii cei mai importanți ai electricității comerciale."],
+	["Y",	"1",	"27",	"2014",	"",	"",	"Sfântul Dumitru",	"Dmitri Ivanovici Mendeleev a fost un chimist rus, recunoscut a fi unul din cei doi chimiști ce au creat independent unul de altul prima variantă a tabelului periodic al elementelor."],
+	["Y",	"1",	"20",	"2014",	"",	"",	"Sfântul Andrei",	"În cinstea fizicianului Andre-Marie Ampere, fizicianului rus Andrei Saharov, precum și în cinstea răzvrătitului Petru, care în a 67 d.H. nu a dorit să fie răstignit ca Isus, cu capul în sus, ci pe o cruce inversată, cu cu capul în jos, mai aproape de Regele nostru, Satan."],
+	["Y",	"03",	"14",	"2014",	"",	"",	"Sfântul Albert",	"Ziua de naștere a fizicianului teoretician Albert Enstein, autorul teoriei relativității. Contribuțiile sale în fizică sunt legate de teoria relativității restrânse (1905), care unesc mecanica cu electromagnetismul, și de teoria relativității generalizate (1915) care extinde principiul relativitatii mișcării neuniforme, elaborând o noua teorie a gravitației."],
+	["Y",	"02",	"14",	"2014",	"",	"",	"Preacuvioasa Sasha",	"Ziua de naștere a simbolului pornografiei moderne, Sasha Grey. Va fi canonizată mai mult datorită activitații sale ateiste (la propunerea Mitropolitului SUA: Leonid Jalenco) "],
+	["Y",	"12",	"25",	"2014",	"",	"",	"Paștele",	"Potrivit calendarului satarean, care este cel mai adevărat calendar, astăzi 25 decembrie este Învierea Mântuitorului Troll. Vă urăm un Paște Fericit !"]
+);
